4310ce6fdd5e781842cdcaf8cfc38db56765cf3c
krishnaChauhan08 <chauhankrishna0820@gmail.com>
2025-02-18 01:12:05 +0530

    Modern Todo App

---
+++ Todo App/app.js
@@ -0,0 +1,80 @@
+const todoForm = document.querySelector("form");
+const todoInput = document.getElementById("todo-input");
+const todoListUL = document.getElementById("todo-list");
+
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
+});
+
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  if (todoText.length > 0) {
+    const todoObject = {
+      text: todoText,
+      completed: false,
+    };
+    allTodos.push(todoObject);
+    updateTodoList();
+    saveTodos();
+    todoInput.value = "";
+  }
+}
+
+function updateTodoList() {
+  todoListUL.innerHTML = "";
+  allTodos.forEach((todo, todoIndex) => {
+    todoItem = createTodoItem(todo, todoIndex);
+    todoListUL.append(todoItem);
+  });
+}
+
+function createTodoItem(todo, todoIndex) {
+  const todoId = "todo-" + todoIndex;
+  const todoLi = document.createElement("li");
+  const todoText = todo.text;
+  todoLi.className = "todo";
+  todoLi.innerHTML = `
+          <input type="checkbox" id="${todoId}" />
+          <label class="custom-checkbox" for="${todoId}">
+            <img src="icons/check.svg" alt="" />
+          </label>
+          <label for="${todoId}" class="todo-text">
+            ${todoText}
+          </label>
+          <button class="delete-btn">
+            <img src="icons/delete.svg" alt="" />
+          </button>
+        </li>
+    `;
+  const deleteButton = todoLi.querySelector(".delete-btn");
+  deleteButton.addEventListener("click", () => {
+    deleteTodoItem(todoIndex);
+  });
+  const checkbox = todoLi.querySelector("input");
+  checkbox.addEventListener("change", () => {
+    allTodos[todoIndex].completed = checkbox.checked;
+    saveTodos();
+  });
+  checkbox.checked = todo.completed;
+  return todoLi;
+}
+
+function deleteTodoItem(todoIndex) {
+  allTodos = allTodos.filter((_, i) => i !== todoIndex);
+  saveTodos();
+  updateTodoList();
+}
+
+function saveTodos() {
+  const todosJson = JSON.stringify(allTodos);
+  localStorage.setItem("todos", todosJson);
+}
+
+function getTodos() {
+  const todos = localStorage.getItem("todos") || "[]";
+  return JSON.parse(todos);
+}
